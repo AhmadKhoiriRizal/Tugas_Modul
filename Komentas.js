21d2731ebbd0b0b024a300e49ce705e559eb7715
AhmadKhoiriRizal <rizalice14@gmail.com>
2023-03-29 11:27:48 +0700

    first tugas

---
+++ Komentas.js
@@ -0,0 +1,7 @@
+//console.log("Ini adalah komen satu baris");
+console.log("tugas komen //");
+
+/*console.log("ini adalah komen dengan lebih dari satu baris");
+console.log("Teks apapun yang berada disini akan dijadikan komen");
+console.log("Ketika menggunakan ini, jangan lupa untuk menutupnya");*/
+console.log("tugas komen /* */");
---
+++ Var, Let, dan Count.js
@@ -0,0 +1,13 @@
+//Bisa ditambah variabel baru dan 
+//variabel lama masih ditampilkan
+var message="Mari Belajar";
+message += " Javascript";
+message += " Untuk Pemula";
+/*Bisa ditambah variabel baru namun
+variabel lama tidak ditampilkan*/
+let nomber=15;
+nomber=50;
+nomber=95;
+//Sifat variabel tetap dan tidak bisa dirubah lagi
+const point=100;
+console.log(message, nomber, point);
